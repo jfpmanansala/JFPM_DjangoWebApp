63d2b08add779e42b862d79f4bd4b630f5d400a8
Janna Freesha Pineda Manansala <jfpmanansala.student@ua.edu.ph>
2026-09-15 20:55:13 +0800

    Allow Vercel host

--- JFPM_DjangoWebApp/JFPM_DjangoWebApp/settings.py
+++ JFPM_DjangoWebApp/JFPM_DjangoWebApp/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-_#q_g%t91!%cw#&r#)d@@^&d^85fa1-685rh!!tg8%ye6ghwfa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".vercel.app",
+    "localhost",
+    "127.0.0.1"
+]
 
 
 # Application definition
